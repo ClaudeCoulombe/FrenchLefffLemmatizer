207217195589220e73bb1252671cd582d277d928
Unknown <bonnjona@hotmail.fr>
2018-04-01 23:29:43 +0200

    test

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup
 
 setup(name='FrenchLefffLemmatizer',
-      version='0.1',
+      version='0.2',
       description='French Lemmatizer based on LEFFF a large-scale morphological and syntactic lexicon for French',
       url='https://github.com/ClaudeCoulombe/FrenchLefffLemmatizer',
       author='Claude Coulombe',
       author_email='claude.coulombe@gmail.com',
       license='Apache 2',
-      packages=['lemmatizer', 'tests'],
-      package_data={
-        'data': ['lefff-3.4.mlex','lefff-3.4-addition.mlex']
-      },
+      packages=['french_lefff_lemmatizer'],
+      data_files=[('data', ['data/*.mlex'])],
+      include_package_data=True,
       zip_safe=False)
 
