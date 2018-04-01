de7c2872627b695a707ecc88616713d51422184e
Unknown <bonnjona@hotmail.fr>
2018-04-01 23:41:29 +0200

    last try

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(name='FrenchLefffLemmatizer',
       version='0.2',
       description='French Lemmatizer based on LEFFF a large-scale morphological and syntactic lexicon for French',
+      keywords='french lefff lemmatizer lexicon',
       url='https://github.com/ClaudeCoulombe/FrenchLefffLemmatizer',
       author='Claude Coulombe',
       author_email='claude.coulombe@gmail.com',
       license='Apache 2',
       packages=['french_lefff_lemmatizer'],
-      package_dir={'french_lefff_lemmatizer': 'lemmatizer'},
       package_data={'french_lefff_lemmatizer': ['data/*.mlex']},
       include_package_data=True,
       zip_safe=False)
